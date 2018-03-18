f07529a72bc2b19a59ee9f73cc96f85af89712d6
Abraham Uribe <abrahamuribejr@gmail.com>
2018-03-17 18:48:39 -0700

    Base code

---
+++ build/motor/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/abraham/mars_rover_ws/src/motor
+# Build directory: /home/abraham/mars_rover_ws/build/motor
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/sensor/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/abraham/mars_rover_ws/src/sensor
+# Build directory: /home/abraham/mars_rover_ws/build/sensor
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
