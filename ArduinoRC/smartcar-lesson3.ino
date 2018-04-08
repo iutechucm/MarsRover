901dd0680a010acf63f51f4d173c535c1f3a8e8f
cyberbean <auribe9@ucmerced.edu>
2018-04-08 12:27:52 -0700

    Add arduino files

---
+++ ArduinoRC/smartcar-lesson3.ino
@@ -0,0 +1,197 @@
+#include <Servo.h>
+#include "configuration.h"
+
+Servo head;
+/*motor control*/
+void go_Advance(void)  //Forward
+{
+  digitalWrite(dir1PinL, HIGH);
+  digitalWrite(dir2PinL,LOW);
+  digitalWrite(dir1PinR,HIGH);
+  digitalWrite(dir2PinR,LOW);
+}
+void go_Left()  //Turn left
+{
+  digitalWrite(dir1PinL, HIGH);
+  digitalWrite(dir2PinL,LOW);
+  digitalWrite(dir1PinR,LOW);
+  digitalWrite(dir2PinR,HIGH);
+}
+void go_Right()  //Turn right
+{
+  digitalWrite(dir1PinL, LOW);
+  digitalWrite(dir2PinL,HIGH);
+  digitalWrite(dir1PinR,HIGH);
+  digitalWrite(dir2PinR,LOW);
+}
+void go_Back()  //Reverse
+{
+  digitalWrite(dir1PinL, LOW);
+  digitalWrite(dir2PinL,HIGH);
+  digitalWrite(dir1PinR,LOW);
+  digitalWrite(dir2PinR,HIGH);
+}
+void stop_Stop()    //Stop
+{
+  digitalWrite(dir1PinL, LOW);
+  digitalWrite(dir2PinL,LOW);
+  digitalWrite(dir1PinR,LOW);
+  digitalWrite(dir2PinR,LOW);
+}
+
+/*set motor speed */
+void set_Motorspeed(int speed_L,int speed_R)
+{
+  analogWrite(speedPinL,speed_L); 
+  analogWrite(speedPinR,speed_R);   
+}
+
+void buzz_ON()   //open buzzer
+{
+  digitalWrite(BUZZ_PIN, LOW);
+}
+void buzz_OFF()  //close buzzer
+{
+  digitalWrite(BUZZ_PIN, HIGH);
+}
+void alarm(){
+   buzz_ON();
+   delay(100);
+   buzz_OFF();
+}
+
+/*detection of ultrasonic distance*/
+int watch(){
+  long howfar;
+  digitalWrite(Trig_PIN,LOW);
+  delayMicroseconds(5);                                                                              
+  digitalWrite(Trig_PIN,HIGH);
+  delayMicroseconds(15);
+  digitalWrite(Trig_PIN,LOW);
+  howfar=pulseIn(Echo_PIN,HIGH);
+  howfar=howfar*0.01657; //how far away is the object in cm
+  //Serial.println((int)howfar);
+  return round(howfar);
+}
+//Meassures distances to the right, left, front, left diagonal, right diagonal and asign them in cm to the variables rightscanval, 
+//leftscanval, centerscanval, ldiagonalscanval and rdiagonalscanval (there are 5 points for distance testing)
+void watchsurrounding(){                     
+  centerscanval = watch();
+  if(centerscanval<distancelimit){
+    stop_Stop();
+    alarm();
+    }
+  head.write(120);
+  delay(100);
+  ldiagonalscanval = watch();
+  if(ldiagonalscanval<distancelimit){
+    stop_Stop();
+    alarm();
+    }
+  head.write(170); //Didn't use 180 degrees because my servo is not able to take this angle
+  delay(300);
+  leftscanval = watch();
+  if(leftscanval<sidedistancelimit){
+    stop_Stop();
+    alarm();
+    }
+  head.write(120);
+  delay(100);
+  ldiagonalscanval = watch();
+  if(ldiagonalscanval<distancelimit){
+    stop_Stop();
+    alarm();
+    }
+  head.write(90); //use 90 degrees if you are moving your servo through the whole 180 degrees
+  delay(100);
+  centerscanval = watch();
+  if(centerscanval<distancelimit){
+    stop_Stop();
+    alarm();
+    }
+  head.write(40);
+  delay(100);
+  rdiagonalscanval = watch();
+  if(rdiagonalscanval<distancelimit){
+    stop_Stop();
+    alarm();
+    }
+  head.write(0);
+  delay(100);
+  rightscanval = watch();
+  if(rightscanval<sidedistancelimit){
+    stop_Stop();
+    alarm();
+    }
+  head.write(90); //Finish looking around (look forward again)
+  delay(300);
+}
+
+void auto_avoidance(){
+  set_Motorspeed(SPEED,SPEED);
+  go_Advance();  // if nothing is wrong go forward using go() function above.
+  ++numcycles;
+  if(numcycles>=1000){ //Watch if something is around every 100 loops while moving forward 
+     stop_Stop();
+    watchsurrounding();
+    if((leftscanval<sidedistancelimit || ldiagonalscanval<distancelimit)&& \
+     (rightscanval>sidedistancelimit || rdiagonalscanval>distancelimit)){
+      go_Right();
+      delay(turntime);
+      stop_Stop();
+    }
+    if((rightscanval<sidedistancelimit || rdiagonalscanval<distancelimit)&&\
+    (leftscanval>sidedistancelimit || ldiagonalscanval>distancelimit)){
+      go_Left();
+      delay(turntime);
+      stop_Stop();
+    }
+    if((rightscanval<sidedistancelimit || rdiagonalscanval<distancelimit)\
+        &&(leftscanval<sidedistancelimit || ldiagonalscanval<distancelimit)\
+        &&(centerscanval<distancelimit)){
+          stop_Stop();
+        }
+    numcycles=0; //Restart count of cycles
+  }
+  distance = watch(); // use the watch() function to see if anything is ahead (when the robot is just moving forward and not looking around it will test the distance in front)
+  if (distance<distancelimit){ // The robot will just stop if it is completely sure there's an obstacle ahead (must test 25 times) (needed to ignore ultrasonic sensor's false signals)
+      ++thereis;}
+  if (distance>distancelimit){
+      thereis=0;} //Count is restarted
+  if (thereis > 25){
+    stop_Stop(); // Since something is ahead, stop moving.
+    thereis=0;
+  }
+}
+
+void setup() {
+  /*setup L298N pin mode*/
+  pinMode(dir1PinL, OUTPUT); 
+  pinMode(dir2PinL, OUTPUT); 
+  pinMode(speedPinL, OUTPUT);  
+  pinMode(dir1PinR, OUTPUT);
+  pinMode(dir2PinR, OUTPUT); 
+  pinMode(speedPinR, OUTPUT); 
+  stop_Stop();//stop move
+  /*init HC-SR04*/
+  pinMode(Trig_PIN, OUTPUT); 
+  pinMode(Echo_PIN,INPUT); 
+  /*init buzzer*/
+  pinMode(BUZZ_PIN, OUTPUT);
+  digitalWrite(BUZZ_PIN, HIGH);  
+  buzz_OFF(); 
+
+  digitalWrite(Trig_PIN,LOW);
+  /*init servo*/
+  head.attach(SERVO_PIN); 
+  head.write(90);
+  /*baud rate*/
+  Serial.begin(9600);
+
+}
+
+void loop() {
+ auto_avoidance();
+}
+
+
